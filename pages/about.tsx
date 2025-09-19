6f5af931b35ddcf0090c1daf5ca4635ec32ab84f
ttt <thanhtungcis60@gmail.com>
2025-09-19 10:56:02 +0700

    NextJS: 02-05 Automatic Static Optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,17 @@
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 // export interface AboutPageProps {}
 
 export default function AboutPage() {
+  const router = useRouter();
+  console.log('About query: ', router.query);
   //props: AboutPageProps
   return <div>About page</div>;
+}
+
+export function getServerSideProps() {
+  return {
+    props: {},
+  };
 }
