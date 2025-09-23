a6fc06cba4bf5d4e5af665685ae1f6b34f5b0e22
ttt <thanhtungcis60@gmail.com>
2025-09-23 13:49:10 +0700

    NextJS: 02-06 SSG getStaticProps with typescript

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,39 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import * as React from 'react';
 
-export interface PostListPageProps {}
-
-export default function PostListPage(props: PostListPageProps) {
-  return <div>Post List Page</div>;
+export interface PostListPageProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  posts: any[];
 }
+
+export default function PostListPage({ posts }: PostListPageProps) {
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext,
+) => {
+  //server-side
+  //build-time
+  console.log('static props');
+  const response = await fetch(
+    'https://json-server-na4j.onrender.com/api/products?_page=1&_limit=10',
+  );
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      posts: data.data.map((x: any) => ({ id: x.id, name: x.name })),
+    },
+  };
+};
