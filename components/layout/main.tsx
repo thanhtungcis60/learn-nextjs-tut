3e927e389897b1a4258b3e9bf3dc911270d86b01
ttt <thanhtungcis60@gmail.com>
2025-10-09 15:56:27 +0700

    NextJS: 04-07 Setup nhiều dạng container cho project

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,5 +1,5 @@
 import { LayoutProps } from '@/models';
-import { Box, Stack } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
 import Link from 'next/link';
 import { Footer, Header } from '../common';
 
@@ -8,6 +8,10 @@ export function MainLayout({ children }: LayoutProps) {
     <Stack minHeight="100vh">
       <Header />
       <Box component="main" flexGrow={1}>
+        <Container maxWidth="sm" sx={{ bgcolor: 'primary.light' }}>
+          SM Container
+        </Container>
+        <Container sx={{ bgcolor: 'primary.light' }}>MD Container</Container>
         <Link href="/">Home</Link>
         <Link href="/blog">Blog</Link>
         <Link href="/works">Works</Link>
