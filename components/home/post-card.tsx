5bd411e492b3fc98d32c248677a63428edcdaf65
ttt <thanhtungcis60@gmail.com>
2025-10-20 15:31:07 +0700

    NextJS: 04-15 Cập nhật tsconfig để hỗ trợ bỏ /index ở cuối import

--- components/home/post-card.tsx
+++ components/home/post-card.tsx
@@ -1,5 +1,5 @@
 import { Post } from '@/models';
-import { Card, CardContent, Divider, Typography } from '@mui/material';
+import { Card, CardContent, Divider, Stack, Typography } from '@mui/material';
 import * as React from 'react';
 import { format } from 'date-fns';
 
@@ -15,12 +15,18 @@ export function PostCard({ post }: PostCardProps) {
         <Typography variant="h5" fontWeight="bold">
           {post.title}
         </Typography>
-        <Typography variant="body1" my={2} sx={{ display: 'flex' }} component="div">
-          {/*Mặc định Typography render ra thẻ p, Divider render thẻ div. P bọc div là không hợp lệ, nên thêm  component="div" để hợp lệ*/}
+        {/*Mặc định Typography render ra thẻ p, Divider render thẻ div. P bọc div là không hợp lệ, nên thêm  component="div" để hợp lệ*/}
+        {/* <Typography variant="body1" my={2} sx={{ display: 'flex' }} component="div">
           {format(Number(post.publishedDate), 'dd MMM yyyy')}
           <Divider orientation="vertical" sx={{ mx: 2 }} flexItem />
           {post.tagList.join(',')}
-        </Typography>
+        </Typography>*/}
+        <Stack direction="row" my={2}>
+          <Typography variant="body1">{format(Number(post.publishedDate), 'dd MMM yyyy')}</Typography>
+          <Divider orientation="vertical" sx={{ mx: 2 }} flexItem />
+          <Typography variant="body1">{post.tagList.join(',')}</Typography>
+        </Stack>
+
         <Typography variant="body2">{post.description}</Typography>
       </CardContent>
     </Card>
