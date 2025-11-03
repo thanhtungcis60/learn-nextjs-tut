200960aca775e5630790aefbf9a2cd32fe8fdd64
ttt <thanhtungcis60@gmail.com>
2025-11-03 16:05:04 +0700

    NextJS: 06-11 Cache logged in user info

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -1,8 +1,20 @@
 import { authApi } from '@/api-client';
-import { LoginPayload } from '@/models';
+import { StorageKeys } from '@/constants';
+import { LoginPayload, UserProfile } from '@/models';
+import { useEffect } from 'react';
 import useSWR from 'swr';
 import { PublicConfiguration } from 'swr/_internal';
 
+function getUserInfo(): UserProfile | null {
+  try {
+    const data = localStorage.getItem(StorageKeys.USER_INFO);
+    if (!data) return null;
+    return JSON.parse(data) as UserProfile;
+  } catch (error) {
+    // console.log('fail to parse user info from local storage', error);
+    return null;
+  }
+}
 export function useAuth(options?: Partial<PublicConfiguration>) {
   const {
     data: profile,
@@ -12,7 +24,24 @@ export function useAuth(options?: Partial<PublicConfiguration>) {
     dedupingInterval: 60 * 60 * 1000, //tự động revalidate trong 1h
     revalidateOnFocus: false, //qua tab khac ko can revalidate
     ...options,
+    onSuccess(data, key, config) {
+      //save user info to local storage
+      localStorage.setItem(StorageKeys.USER_INFO, JSON.stringify(data));
+    },
+    onError(err, key, config) {
+      //failed to get profile
+      console.log(err);
+      logout();
+    },
   });
+
+  useEffect(() => {
+    const localUser = getUserInfo();
+    if (localUser && !profile) {
+      mutate(localUser, false); // set data nhưng không trigger revalidate
+    }
+  }, []);
+
   console.log({ profile, error });
 
   const firstLoading = profile === undefined && error === undefined;
@@ -23,6 +52,7 @@ export function useAuth(options?: Partial<PublicConfiguration>) {
   async function logout() {
     await authApi.logout();
     mutate({}, false); //xoa profile di, ko can goi lai api /profile
+    localStorage.removeItem(StorageKeys.USER_INFO);
   }
 
   return {
--- models/auth.ts
+++ models/auth.ts
@@ -2,3 +2,7 @@ export interface LoginPayload {
   username: string;
   password: string;
 }
+export interface UserProfile {
+  username: string;
+  city: string;
+}
